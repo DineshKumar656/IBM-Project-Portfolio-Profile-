e5cb956fc3bc1ea12aa701cd3e132e6a86a2d660
DineshKumar656 <dineshkumar53492@gmail.com>
2025-10-02 15:20:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/IBM-Project-Portfolio-Profile-/", // 👈 Add your repo name here
   server: {
     host: "::",
     port: 8080,
